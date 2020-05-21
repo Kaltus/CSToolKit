1951b06dd3cb1b7b1215c94e6e7dffa140b5f0b8
chengshu1993 <844547689@qq.com>
2020-05-21 15:47:35 +0800

    1：新增base64编码，SHA编码，MD5编码

--- CSToolKit/Category/NSString+CSString.h
+++ CSToolKit/Category/NSString+CSString.h
@@ -10,6 +10,20 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+typedef NS_ENUM(NSInteger,SHAEncodingType) {
+    
+    SHA1Encoding,
+    
+    SHA224Encoding,
+    
+    SHA256Encoding,
+    
+    SHA384Encoding,
+    
+    SHA512Encoding
+    
+};
+
 @interface NSString (CSString)
 
 /*
@@ -17,6 +31,37 @@ NS_ASSUME_NONNULL_BEGIN
  * @return 转换后的对象
  */
 -(id)jsonConvertObject:(NSStringEncoding)encoding;
+
+
+/*
+ * @description base64编码加密
+ * @param string 待编码加密的字符串
+ * @param option 加密选项 默认为 NSDataBase64Encoding64CharacterLineLength（0）
+ * @return 加密后的编码字符串
+ */
+-(NSString *)base64Encoding:(NSDataBase64EncodingOptions)option;
+
+/*
+ * @description base64解码
+ * @param string 待解码的字符串
+ * @param option 解码选项
+ * @return 解码后的字符串
+ */
+-(NSString *)base64Decoding:(NSDataBase64DecodingOptions)option;
+
+/*
+ * @description md5的编码
+ * @param string 待编码的字符串
+ * @return md5编码后的字符串
+ */
+-(NSString *)md5Encoding;
+
+/*
+ * @description sha编码
+ * @description shaEncodingType sha编码类型
+ * @return sha编码
+ */
+-(NSString *)SHAEncoding:(SHAEncodingType)shaEncodingType;
 
 @end
 
--- CSToolKit/Category/NSString+CSString.m
+++ CSToolKit/Category/NSString+CSString.m
@@ -7,9 +7,11 @@
 //
 
 #import "NSString+CSString.h"
+#import <CommonCrypto/CommonDigest.h>
 
 @implementation NSString (CSString)
 
+///json字符串转换为对象
 -(id)jsonConvertObject:(NSStringEncoding)encoding {
     
     NSData *data = [self dataUsingEncoding:encoding];
@@ -36,5 +38,115 @@
         
     }
 }
+
+///对字符串进行base64编码处理
+-(NSString *)base64Encoding:(NSDataBase64EncodingOptions)option {
+    
+    NSData *data =[self dataUsingEncoding:NSUTF8StringEncoding];
+    
+     return [data base64EncodedStringWithOptions:option];
+}
+
+///对base64字符串进行解码
+-(NSString *)base64Decoding:(NSDataBase64DecodingOptions)option {
+    
+    NSData *data = [[NSData alloc] initWithBase64EncodedString:self options:option];
+  
+    return [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding];
+    
+}
+
+///MD5编码
+-(NSString *)md5Encoding {
+    
+    const char *fooData = [self UTF8String];
+
+    unsigned char result[CC_MD5_DIGEST_LENGTH];
+
+    CC_MD5(fooData, (CC_LONG)strlen(fooData), result);
+
+    NSMutableString *saveResult = [NSMutableString string];
+
+    for (int i = 0; i < CC_MD5_DIGEST_LENGTH; i++) {
+
+    [saveResult appendFormat:@"%02x", result[i]];
+
+    }
+
+    return saveResult;
+}
+
+///SHA 编码
+-(NSString *)SHAEncoding:(SHAEncodingType)shaEncodingType {
+    
+    const char *fooData = [self UTF8String];
+
+    NSMutableString *saveResult = [NSMutableString string];
+
+    switch (shaEncodingType) {
+        case SHA1Encoding:{
+            unsigned char result[CC_SHA1_DIGEST_LENGTH];
+            CC_SHA1(fooData,(CC_LONG)strlen(fooData),result);
+            
+            for (int i = 0; i < CC_SHA1_DIGEST_LENGTH; i++) {
+
+               [saveResult appendFormat:@"%02x", result[i]];
+
+            }
+            
+        }break;
+        case SHA224Encoding:{
+            unsigned char result[CC_SHA224_DIGEST_LENGTH];
+            
+            CC_SHA224(fooData,(CC_LONG)strlen(fooData),result);
+                   
+            for (int i = 0; i < CC_SHA224_DIGEST_LENGTH; i++) {
+
+                [saveResult appendFormat:@"%02x", result[i]];
+
+            }
+                   
+        }break;
+        case SHA256Encoding:{
+            unsigned char result[CC_SHA256_DIGEST_LENGTH];
+            CC_SHA256(fooData,(CC_LONG)strlen(fooData),result);
+            
+            for (int i = 0; i < CC_SHA256_DIGEST_LENGTH; i++) {
+
+               [saveResult appendFormat:@"%02x", result[i]];
+
+            }
+            
+        }break;
+        case SHA384Encoding:{
+            unsigned char result[CC_SHA384_DIGEST_LENGTH];
+            CC_SHA384(fooData,(CC_LONG)strlen(fooData),result);
+            
+            for (int i = 0; i < CC_SHA384_DIGEST_LENGTH; i++) {
+
+               [saveResult appendFormat:@"%02x", result[i]];
+
+            }
+            
+        }break;
+        case SHA512Encoding:{
+            unsigned char result[CC_SHA512_DIGEST_LENGTH];
+            CC_SHA512(fooData, (CC_LONG)strlen(fooData), result);
+            
+            for (int i = 0; i < CC_SHA512_DIGEST_LENGTH; i++) {
+
+               [saveResult appendFormat:@"%02x", result[i]];
+
+            }
+            
+        }break;
+        default:
+            break;
+    }
+    
+    return saveResult;
+    
+}
+
 
 @end
